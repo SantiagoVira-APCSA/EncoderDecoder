c9b12c2b6599657b3a2499d4d2d1fe8d638b0baa
SantiagoVira <santiagovira06@gmail.com>
2022-11-30 11:54:54 -0500

    JavaDocs

---
+++ docs/type-search-index.js
@@ -0,0 +1,5 @@
+typeSearchIndex = [{"l": "All Classes and Interfaces", "u": "allclasses-index.html"}, {
+    "p": "<Unnamed>",
+    "l": "Decoder"
+}, {"p": "<Unnamed>", "l": "Encoder"}, {"p": "<Unnamed>", "l": "Main"}];
+updateSearchResults();
